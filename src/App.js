69f5d2f942c7ca042a8ead8c11bef71419c5659e
= <natanael926@gmail.com>
2023-04-25 13:17:48 -0400

    persistir los datos en localstorage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import './App.css';
+import { useState } from "react"
 import TaskContainer from './componets/TaskContainer';
-import { CreateTodoButton } from './componets/CreateTodoButton';
 import CreateTask from './componets/CreateTask';
 
-
-
 function App() {
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+    parsedTodos = []
+  } else {
+    parsedTodos =  JSON.parse(localStorageTodos)
+  }
+
+  const [searchValue, setValueSearch] = useState('')
+  const [todos, setTodos] = useState(parsedTodos)
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length
+  const totalTodos = todos.length
+
+  let searchedTodos = []
+
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos
+  } else {
+    searchedTodos = todos.filter(todo => {
+        const searchText = searchValue.toLowerCase()
+        const todoText = todo.text.toLowerCase()
+        return todoText.includes(searchText)
+    })
+  }
+
+  const saveTodo = (newTodo) => {
+    const stringifiedTodo = JSON.stringify(newTodo)
+    localStorage.setItem('TODOS_V1', stringifiedTodo)
+    setTodos(newTodo)
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodo = [...todos];
+    
+    newTodo[todoIndex].completed = true;
+    saveTodo(newTodo)
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodo = [...todos]
+    
+    newTodo.splice(todoIndex, 1)
+    saveTodo(newTodo)
+  }
+
   return (
     <>
-
       <CreateTask />
-      <TaskContainer />
-
-      
-      {/* <CreateTodoButton /> */}
+      <TaskContainer
+        totalTodos={totalTodos}
+        completedTodos={completedTodos}
+        searchValue={searchValue}
+        setValueSearch={setValueSearch}
+        searchedTodos={searchedTodos}
+        completeTodo={completeTodo}
+        deleteTodo={deleteTodo}
+      />
     </>
   );
 }
--- src/componets/TaskContainer.jsx
+++ src/componets/TaskContainer.jsx
@@ -1,53 +1,22 @@
-import { useState } from "react"
 import { TodoCounter } from "./TodoCounter"
 import { TodoSearch } from "./TodoSearch"
 import { TodoList } from "./TodoList"
 import { TodoItem } from "./TodoItem"
 
-function TaskContainer() {
-
-    const todosDefault = [
-        {text: 'Cortar cebolla', completed: true},
-        {text: 'Cortar cebolla 2', completed: false},
-        {text: 'Cortar cebolla 3', completed: false},
-      ]
- 
-    const [searchValue, setValueSearch] = useState('')
-    const [todos, setTodos] = useState(todosDefault)
-
-    const completedTodos = todos.filter(todo => !!todo.completed).length
-    const tatalTodos = todos.length
-
-    let searchedTodos = []
-
-    if(!searchValue.length >= 1) {
-        searchedTodos = todos
-    } else {
-        searchedTodos = todos.filter(todo => {
-            const searchText = searchValue.toLowerCase()
-            const todoText = todo.text.toLowerCase()
-            return todoText.includes(searchText)
-        })
-    }
-
-    const completeTodo = (text) => {
-        const todoIndex = todos.findIndex(todo => todo.text === text);
-        const newTodo = [...todos];
-        newTodo[todoIndex].completed = true;
-        setTodos(newTodo);
-    }
-
-    const deleteTodo = (text) => {
-        const todoIndex = todos.findIndex(todo => todo.text === text);
-        const newTodo = [...todos]
-        newTodo.splice(todoIndex, 1)
-        setTodos(newTodo);
-    }
+function TaskContainer({
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setValueSearch,
+    searchedTodos,
+    completeTodo,
+    deleteTodo
+}) {
 
     return ( 
         <section className='taskContainer'>
         <TodoCounter
-            total={tatalTodos}
+            total={totalTodos}
             completed={completedTodos}
         />
         <TodoSearch 
